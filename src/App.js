d41c72f34e2316f112a4c759f642bb0e9ba47302
Bogdan Kaleniuk <bogdankaleniuk95@gmail.com>
2024-05-22 16:30:56 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,233 @@
+import React from "react";
+import "./App.css";
+import { ReactComponent as Sprite } from "./sprite.svg";
+
+const App = () => {
+  return (
+    <div className="home-repairs">
+      <div className="repair-categories">
+        <div className="repair-category">
+          <Sprite style={{ display: "none" }} />
+          <svg className="color-icon" width="94" height="100">
+            <use xlinkHref="#svg1"></use>
+          </svg>
+          <div className="repair-category-content">
+            <h3 className="title-content">Plumbing</h3>
+            <ul>
+              <li>Install / Replace a Toilet</li>
+              <li>Install/Replace a Faucet</li>
+              <li>Fix a Leaking</li>
+              <li>Install/Replace a Sink</li>
+              <li>Repair Under Sink Plumbing</li>
+              <li>
+                <a
+                  href="https://portfoliokaleniukbogdan.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View more
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="repair-category">
+          <Sprite style={{ display: "none" }} />
+          <svg className="color-icon" width="91" height="100">
+            <use xlinkHref="#svg2"></use>
+          </svg>
+          <div className="repair-category-content">
+            <h3 className="title-content">Drywall & painting</h3>
+            <ul>
+              <li>Install Drywall</li>
+              <li>Patch and Paint Drywall</li>
+              <li>Paint a Wall/Trim</li>
+              <li>Paint or Stain A Fence</li>
+              <li>Paint a Cabinet Door</li>
+              <li>
+                <a
+                  href="https://portfoliokaleniukbogdan.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View more
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="repair-category">
+          <Sprite style={{ display: "none" }} />
+          <svg className="color-icon" width="123" height="100">
+            <use xlinkHref="#svg3"></use>
+          </svg>
+          <div className="repair-category-content">
+            <h3 className="title-content">Furniture assembly</h3>
+            <ul>
+              <li>Assemble Fitness Equipment</li>
+              <li>Assemble - Custom Request</li>
+              <li>Assemble a TV Stand</li>
+              <li>Assemble Pet Furniture</li>
+              <li>Assemble a Bar Cabinet</li>
+              <li>
+                <a
+                  href="https://portfoliokaleniukbogdan.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View more
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="repair-category">
+          <Sprite style={{ display: "none" }} />
+          <svg className="color-icon" width="111" height="100">
+            <use xlinkHref="#svg4"></use>
+          </svg>
+          <div className="repair-category-content">
+            <h3 className="title-content">Electrical</h3>
+            <ul>
+              <li>Install / Replace a Ceiling Fan</li>
+              <li>Replace Switch & Outlet Faceplate</li>
+              <li>Upgrade to a Video Doorbell</li>
+              <li>Install / Replace an Bathroom Exhaust Fan</li>
+              <li>
+                <a
+                  href="https://portfoliokaleniukbogdan.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View more
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="repair-category">
+          <Sprite style={{ display: "none" }} />
+          <svg className="color-icon" width="110" height="100">
+            <use xlinkHref="#svg5"></use>
+          </svg>
+          <div className="repair-category-content">
+            <h3 className="title-content">Doors & windows</h3>
+            <ul>
+              <li>Install / Replace New Door</li>
+              <li>Install / Replace a Screen Door</li>
+              <li> Install / Replace a Window Screen</li>
+              <li>Repair a Door Frame</li>
+
+              <li>
+                <a
+                  href="https://portfoliokaleniukbogdan.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View more
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="repair-category">
+          <Sprite style={{ display: "none" }} />
+          <svg className="color-icon" width="117" height="100">
+            <use xlinkHref="#svg6"></use>
+          </svg>
+          <div className="repair-category-content">
+            <h3 className="title-content">Hanging & mounting</h3>
+            <ul>
+              <li>Mount a TV</li>
+              <li>Remove a TV Mount from Wall</li>
+              <li>Mount and Hang a Projector Screen</li>
+              <li>Hang a Bathroom Fixture</li>
+              <li>
+                <a
+                  href="https://portfoliokaleniukbogdan.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View more
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="repair-category">
+          <Sprite style={{ display: "none" }} />
+          <svg className="color-icon" width="98" height="100">
+            <use xlinkHref="#svg7"></use>
+          </svg>
+          <div className="repair-category-content">
+            <h3 className="title-content">Appliance installation</h3>
+            <ul>
+              <li>Replace an Existing Dishwasher</li>
+              <li>Replace an Existing Microwave</li>
+              <li>Replace an Existing Oven</li>
+              <li>
+                <a
+                  href="https://portfoliokaleniukbogdan.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View more
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="repair-category">
+          <Sprite style={{ display: "none" }} />
+          <svg className="color-icon" width="116" height="100">
+            <use xlinkHref="#svg8"></use>
+          </svg>
+          <div className="repair-category-content">
+            <h3 className="title-content">Smart home</h3>
+            <ul>
+              <li>Install a Smart Lock</li>
+              <li>Install a Smart Home Security Camera</li>
+              <li>Mount a Baby Monitor Camera on the Wall</li>
+              <li>Install an Under Sink Water Filtration System </li>
+              <li>
+                <a
+                  href="https://portfoliokaleniukbogdan.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View more
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="repair-category">
+          <Sprite style={{ display: "none" }} />
+          <svg className="color-icon" width="94" height="100">
+            <use xlinkHref="#svg9"></use>
+          </svg>
+          <div className="repair-category-content">
+            <h3 className="title-content">Outdoor</h3>
+            <ul>
+              <li>Assemble an Outdoor Trampoline</li>
+              <li>Hang a Garage Shelving Unit</li>
+              <li>Install Outdoor Decorative Exterior Window Shutters</li>
+              <li>Install a Storm Door</li>
+              <li>
+                <a
+                  href="https://portfoliokaleniukbogdan.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View more
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
